ca158d06b413074c94c8a6b818d67912d3c04663
Achraf Ahrach <achrafahrach44@gmail.com>
2024-03-23 18:36:10 +0000

    probleme container mariadb exit alone

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,8 +1,4 @@
 
-sed -i 's/^# port = 3306/port = 3306/' /etc/mysql/my.cnf
-
-sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
 service mariadb start
 
 sleep 2
@@ -11,7 +7,5 @@ mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PAS
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-
-# service mariadb stop
 
 mariadbd
